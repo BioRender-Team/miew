6ced5a58121b4147f9b82ec43386843a9709d622
Nadezhda Katcman <Nadezhda_Katcman@epam.com>
2019-12-18 18:23:47 +0300

    Add 'zoom' and 'rotate' event listners

--- examples/events.js
+++ examples/events.js
@@ -8,4 +8,20 @@
   if (viewer.init()) {
     viewer.run();
   }
+
+  const ul = document.getElementById('events');
+
+  viewer.addEventListener('rotate', (e) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode('rotate: ' + e.angle));
+    li.style.backgroundColor = '#ffca99';
+    ul.appendChild(li);
+  });
+
+  viewer.addEventListener('zoom', (e) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode('factor: ' + e.factor));
+    li.style.backgroundColor = '#bdffc4';
+    ul.appendChild(li);
+  });
 }());
